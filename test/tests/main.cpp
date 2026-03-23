758aaf30f46d2daeae5c2a8a7970f7c137052457
MarkVL <v.o.markin221@gmail.com>
2026-03-23 16:23:59 +0300

    update fcfTest

--- test/tests/main.cpp
+++ test/tests/main.cpp
@@ -10,8 +10,8 @@ FCF_TEST_GROUP_ORDER("Specificator", -8)
 FCF_TEST_GROUP_ORDER("Variant", -7)
 
 int main(int a_argc, char* a_argv[]) {
-
-  fcf::NTest::Options testOptions;
-  fcf::NTest::cmdRun(testOptions, a_argc, (const char**)a_argv, fcf::NTest::CRM_RUN); 
-  return 0;
+  bool error = false;
+  fcf::NTest::cmdRun(a_argc, (const char**)a_argv, fcf::NTest::CRM_RUN, &error); 
+  return error ? 1 : 0;
 }
+
