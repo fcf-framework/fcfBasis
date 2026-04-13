9fb0a2f44d53aeaa454ce2bea19bcf22e3a8f346
MarkVL <v.o.markin221@gmail.com>
2026-04-13 13:27:36 +0300

    Update foreach demonstration example with new test cases and improved code structure

--- examples/fcf_basis_foreach/main.cpp
+++ examples/fcf_basis_foreach/main.cpp
@@ -24,6 +24,18 @@ struct TuplePrinterSpec {
 
 FCF_FOREACH_METHOD_WRAPPER(TuplePrinterSpecWrapper, TuplePrinterSpec, handler);
 
+struct TupleNumTypeCouner {
+  template<typename TItem>
+  void handler(size_t /*a_index*/, TItem& /*a_item*/, size_t& a_counter){
+    if (fcf::Type<TItem>().name() == "int" || 
+        fcf::Type<TItem>().name() == "float" || 
+        fcf::Type<TItem>().name() == "double"){
+      ++a_counter;
+    }
+  }
+};
+
+FCF_FOREACH_METHOD_WRAPPER(TupleNumTypeCounerWrapper, TupleNumTypeCouner, handler);
 /**
  * @brief Демонстрационное приложение для изучения работы fcf::foreach.
  *
@@ -36,73 +48,90 @@ FCF_FOREACH_METHOD_WRAPPER(TuplePrinterSpecWrapper, TuplePrinterSpec, handler);
 
 int main() {
     std::cout << "=== [1] Работа с последовательными контейнерами (Vector, List) ===" << std::endl;
+    {
+      std::vector<int> vec = {10, 20, 30, 40, 50};
+      std::list<std::string> lst = {"Apple", "Banana", "Cherry"};
 
-    std::vector<int> vec = {10, 20, 30, 40, 50};
-    std::list<std::string> lst = {"Apple", "Banana", "Cherry"};
+      // Обход вектора: получаем индекс и значение
+      fcf::foreach(vec, [](size_t index, const int& value) {
+          std::cout << "Vector element [" << index << "]: " << value << std::endl;
+      });
 
-    // Обход вектора: получаем индекс и значение
-    fcf::foreach(vec, [](size_t index, const int& value) {
-        std::cout << "Vector element [" << index << "]: " << value << std::endl;
-    });
+      std::cout << "\n---" << std::endl;
 
-    std::cout << "\n---" << std::endl;
-
-    // Обход списка: индекс здесь также инкрементируется
-    fcf::foreach(lst, [](size_t index, const std::string& value) {
-        std::cout << "List element [" << index << "]: " << value << std::endl;
-    });
+      // Обход списка: индекс здесь также инкрементируется
+      fcf::foreach(lst, [](size_t index, const std::string& value) {
+          std::cout << "List element [" << index << "]: " << value << std::endl;
+      });
+    }
 
 
     std::cout << "\n=== [2] Работа с ассоциативными контейнерами (Map) ===" << std::endl;
+    {
+      std::map<std::string, double> prices = {
+          {"Coffee", 2.5},
+          {"Tea", 1.5},
+          {"Cake", 5.0}
+      };
 
-    std::map<std::string, double> prices = {
-        {"Coffee", 2.5},
-        {"Tea", 1.5},
-        {"Cake", 5.0}
-    };
-
-    // В map 'index' фактически является ключом (key)
-    fcf::foreach(prices, [](const std::string& key, const double& value) {
-        std::cout << "Product: " << key << " | Price: $" << value << std::endl;
-    });
+      // В map 'index' фактически является ключом (key)
+      fcf::foreach(prices, [](const std::string& key, const double& value) {
+          std::cout << "Product: " << key << "    \t| Price: $" << value << std::endl;
+      });
+    }
 
 
     std::cout << "\n=== [3] Работа с множествами (Set) ===" << std::endl;
+    {
+      std::set<int> unique_numbers = {1, 5, 2, 8, 5}; // 5 повторится один раз
 
-    std::set<int> unique_numbers = {1, 5, 2, 8, 5}; // 5 повторится один раз
-
-    // В set 'index' и 'value' будут указывать на один и тот же элемент
-    fcf::foreach(unique_numbers, [](int index, int value) {
-        std::cout << "Set element: " << value << " (index/key: " << index << ")" << std::endl;
-    });
+      // В set 'index' и 'value' будут указывать на один и тот же элемент
+      fcf::foreach(unique_numbers, [](int index, int value) {
+          std::cout << "Set element: " << value << " (index/key: " << index << ")" << std::endl;
+      });
+    }
 
 
     std::cout << "\n=== [4] Работа с кортежами (Tuple) ===" << std::endl;
-    auto my_tuple = std::make_tuple(1, "Hello", 3.14, 'A');
-    TuplePrinter printer;
-    // foreach для tuple использует метапрограммирование для развертки элементов
-    fcf::foreach(my_tuple, printer);
+    {
+      auto tuple = std::make_tuple(1, "Hello", 3.14, 'A');
+      TuplePrinter handler;
+      fcf::foreach(tuple, handler);
+    }
 
 
     std::cout << "\n=== [5] Работа с кортежами (Tuple) ===" << std::endl;
-    std::tuple<int, std::string, int, char> spec_tuple = std::make_tuple(1, "Hello", 3.14, 'A');
-    TuplePrinterSpec spec_printer;
-    TuplePrinterSpecWrapper spec_printer_wrapper(spec_printer);
-    // foreach для tuple использует метапрограммирование для развертки элементов
-    fcf::foreach(spec_tuple, spec_printer_wrapper);
+    {
+      std::tuple<int, std::string, int, char> tuple = std::make_tuple(1, "Hello", 3.14, 'A');
+      TuplePrinterSpec handler;
+      TuplePrinterSpecWrapper wrapper(handler);
+      fcf::foreach(tuple, wrapper);
+    }
 
-    std::cout << "\n=== [6] Модификация данных через foreach ===" << std::endl;
+    std::cout << "\n=== [6] Работа с кортежами (Tuple) ===" << std::endl;
+    {
+      std::tuple<int, std::string, float, char> tuple = std::make_tuple(1, "Hello", 3.14, 'A');
+      TupleNumTypeCouner handler;
+      TupleNumTypeCounerWrapper wrapper(handler);
+      size_t numCounter = 0;
+      fcf::foreach(tuple, wrapper, numCounter);
+      std::cout << "Количество числовых элементов в кортеже:" << numCounter << std::endl;
+    }
 
-    std::vector<int> numbers = {1, 2, 3, 4, 5};
 
-    // Если передать не const контейнер, можно изменять элементы
-    fcf::foreach(numbers, [](size_t /*index*/, int& value) {
-        value *= 10; // Умножаем каждое число на 10
-    });
+    std::cout << "\n=== [7] Модификация данных через foreach ===" << std::endl;
+    {
+      std::vector<int> numbers = {1, 2, 3, 4, 5};
 
-    std::cout << "Modified vector: ";
-    for(int n : numbers) std::cout << n << " ";
-    std::cout << std::endl;
+      // Если передать не const контейнер, можно изменять элементы
+      fcf::foreach(numbers, [](size_t /*index*/, int& value) {
+          value *= 10; // Умножаем каждое число на 10
+      });
+
+      std::cout << "Modified vector: ";
+      for(int n : numbers) std::cout << n << " ";
+      std::cout << std::endl;
+    }
 
     return 0;
 }
