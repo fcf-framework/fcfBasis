59af70cd437d0b31e7a9b138d03bc46587106dd1
MarkVL <v.o.markin221@gmail.com>
2026-06-13 23:47:00 +0300

    Add documentation comments to the SpecificatorInfo structure in SpecificatorInfo.hpp

--- bits/PartSpecificator/SpecificatorInfo.hpp
+++ bits/PartSpecificator/SpecificatorInfo.hpp
@@ -5,9 +5,28 @@
 
 namespace fcf{
 
+  /**
+   * @brief Structure containing information about a specific part's operator.
+   * 
+   * This structure stores pointers to functions used for executing 
+   * operations specific to a particular part, supporting both 
+   * specialized and universal calling mechanisms.
+   */
   struct SpecificatorInfo {
-    UniversalCall universalCall;
+    /**
+     * @brief Pointer to a specialized function.
+     * 
+     * Used for direct, high-performance execution of part-specific logic.
+     */
     void*         call;
+
+    /**
+     * @brief Universal call function wrapper.
+     * 
+     * Provides a standardized interface for invoking the operator 
+     * through the framework's universal calling mechanism.
+     */
+    UniversalCall universalCall;
   };
 
 } // fcf namespace
